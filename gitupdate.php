e1fb5a4da25ea3b302ac29e6dd0296e3c07b89a8
julianromera <julian.romera@gmail.com>
2014-05-12 02:10:56 +0200

    (git hook): testing webhook

--- gitupdate.php
+++ gitupdate.php
@@ -2,9 +2,9 @@
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
+//if ( $_POST['payload'] ) {
   shell_exec( 'cd /www/agrotest1 && git reset --hard HEAD && git pull origin master' );
-}
+//}
 
 ?>hi
 
